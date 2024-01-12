214b08bc0b07986d16fadcac6ca289772198b64c
John Mark Manuel <manueljm016@gmail.com>
2024-01-12 18:51:13 +0800

    Prep3 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        Console.Write("What is your guess? ");
+        string guessNumberString = Console.ReadLine();
+        int guessNumber = int.Parse(guessNumberString);
+        int attemps = 1;
+
+        while (magicNumber != guessNumber)
+        {
+            if (magicNumber < guessNumber) 
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.Write("What is the magic number? ");
+            guessNumberString = Console.ReadLine();
+            guessNumber = int.Parse(guessNumberString);
+            attemps++;
+        }
+
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"Your number of attemp(s) is {attemps}");
     }
 }
