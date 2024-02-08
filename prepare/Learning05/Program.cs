0c859d4efc865ee454da4c1e81d38ac018c1f148
John Mark Manuel <manueljm016@gmail.com>
2024-02-08 21:18:34 +0800

    Learning05 Completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square1 = new Square("blue", 50);
+        Rectangle rectangle1 = new Rectangle("black", 100, 60);
+        Circle circle1 = new Circle("red", 30);
+        
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine("");
+        }
     }
 }
